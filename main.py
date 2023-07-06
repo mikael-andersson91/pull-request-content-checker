6e9b9628231429c3c11821affbf95ec098ecb504
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2023-07-06 19:50:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import os
 import requests  # noqa We are just importing this to prove the dependency installed correctly
 
 
-# Set the output value by writing to the outputs in the Environment File, mimicking the behavior defined here:
-#  https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter
+# Set the output value by writing to the outputs in the 
+# GITHUB_OUTPUT Environment File 
 def set_github_action_output(output_name, output_value):
     f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]), "a")
     f.write(f'{output_name}={output_value}')
