0e9a52bfe8b8addd358d786266363213172b3f48
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2023-07-06 14:17:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ def main():
 
     my_output = f'Hello {my_input}'
 
+    for line in os.path.abspath(os.environ["GITHUB_OUTPUT"]):
+        print(line)    
+
     print(f'echo "myOutput={my_output}" >> "{os.path.abspath(os.environ["GITHUB_OUTPUT"])}"')
     for line in os.path.abspath(os.environ["GITHUB_OUTPUT"]):
         print(line)    
