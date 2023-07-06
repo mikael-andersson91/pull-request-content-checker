c00e608689b845e929e1274c1c899f5a1cf513b6
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2023-07-06 14:16:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,11 @@ import requests  # noqa We are just importing this to prove the dependency insta
 def main():
     my_input = os.environ["INPUT_MYINPUT"]
 
-    my_output = f"Hello {my_input}"
+    my_output = f'Hello {my_input}'
 
     print(f'echo "myOutput={my_output}" >> "{os.path.abspath(os.environ["GITHUB_OUTPUT"])}"')
+    for line in os.path.abspath(os.environ["GITHUB_OUTPUT"]):
+        print(line)    
 
 
 if __name__ == "__main__":
