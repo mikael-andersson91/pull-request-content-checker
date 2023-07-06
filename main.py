818c759504eb628155ce66440ebacbf3f80ccc10
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2023-07-06 14:22:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ def main():
     my_output = f'Hello {my_input}'
 
     for line in os.path.abspath(os.environ["GITHUB_OUTPUT"]):
-        print(line)    
-    print(os.path.abspath(os.environ["GITHUB_OUTPUT"]).read()) 
-    print(f'echo "myOutput={my_output}" >> "{os.path.abspath(os.environ["GITHUB_OUTPUT"])}"')
-    print(os.path.abspath(os.environ["GITHUB_OUTPUT"]).read()) 
+        print(line) 
+    f = open(os.path.abspath(os.environ["GITHUB_OUTPUT"]),"a") 
+    f.write("myOutput={my_output}")
+    f.close()
+    #print(f'echo "myOutput={my_output}" >> "{os.path.abspath(os.environ["GITHUB_OUTPUT"])}"') 
 
 
 if __name__ == "__main__":
