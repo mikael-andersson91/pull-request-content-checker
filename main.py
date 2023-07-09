c1978e8057cb0dcc86bbe6fadc4bc19dbd42cb90
Mikael Andersson <mikael.andersson3@ingka.ikea.com>
2023-07-09 07:22:30 +0200

    feat: get inputs for pr_template_path and body details

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
 import json
+import difflib
 
 # Set the output value by writing to the outputs in the
 # GITHUB_OUTPUT Environment File
@@ -9,19 +10,26 @@ def set_github_action_output(output_name, output_value):
     f.close()
 
 
-def main():
-    my_input = os.environ["INPUT_MYINPUT"]
-    my_output = f'Hello {my_input}'
 
+def main():
+    pr_template_path = os.environ["INPUT_PULL_REQUEST_TEMPLATE_PATH"]
+    max_pull_request_description_match = os.environ["INPUT_MAX_PULL_REQUEST_DESCRIPTION_MATCH"]
+    print(f'pr_template_path: {pr_template_path}')
+    print(f'max_pull_request_description_match: {max_pull_request_description_match}')
+    
     print(os.environ["GITHUB_EVENT_NAME"])
     f = open(os.environ["GITHUB_EVENT_PATH"])
     event_data = json.load(f)
     f.close()
 
-    print(event_data["pull_request"]["body"])
-    print(event_data["pull_request"]["title"])
+    pr_body = event_data["pull_request"]["body"].strip()
+    pr_title = print(event_data["pull_request"]["title"]).strip()
+    print(pr_body)
+    print(pr_title)
 
-    set_github_action_output('myOutput', my_output)
+    pr_body_similarity_score = difflib.SequenceMatcher(None, pr_body, "test").ratio()
+
+    set_github_action_output('myOutput', pr_body_similarity_score)
 
 
 if __name__ == "__main__":
